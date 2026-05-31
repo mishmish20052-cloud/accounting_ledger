7d0a27c4aaa1b93f252c45e119f92a0ac3949a1f
mishmish20052-cloud <mishmish20052@gmail.com>
2026-05-31 18:00:48 +0300

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String appName = 'دفتر الأستاذ المحاسبي';
+  static const String pinKey = 'user_pin_code';
+  static const String authStatusKey = 'auth_status';
+  static const String typeDebit = 'debit';   // لي
+  static const String typeCredit = 'credit'; // علي
+}
