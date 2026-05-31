0e6cbfe49312317eaff4be5d0d3db9828ed3938a
mishmish20052-cloud <mishmish20052@gmail.com>
2026-06-01 00:52:43 +0300

    Update settings_controller.dart

--- lib/controllers/settings_controller.dart
+++ lib/controllers/settings_controller.dart
@@ -1,4 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../views/screens/settings_screen.dart';
 
-// هذا الملف يقوم بربط الـ Provider لكي يراه ملف app.dart بنجاح دون أخطاء
+// Settings controller for riverpod state
