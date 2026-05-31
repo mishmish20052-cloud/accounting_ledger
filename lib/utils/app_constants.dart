31551c7cf8fad84d6ff6764d562c7da1458088b8
mishmish20052-cloud <mishmish20052@gmail.com>
2026-05-31 19:11:48 +0300

    Create app_constants.dart

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String appName = 'دفتر الأستاذ المحاسبي';
+  static const String pinKey = 'user_pin_code';
+  static const String languageKey = 'app_language_code';
+  static const String themeKey = 'app_theme_mode';
+  
+  // مفاتيح أنواع الحركات المالية
+  static const String typeDebit = 'debit';   // له / مدين
+  static const String typeCredit = 'credit'; // عليه / دائن
+}
