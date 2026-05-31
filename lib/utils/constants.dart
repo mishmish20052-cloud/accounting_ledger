c07fbd6529d3e8c037205f6512f8881f6719a317
mishmish20052-cloud <mishmish20052@gmail.com>
2026-05-31 20:19:59 +0300

    Delete lib/utils/constants.dart

--- lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String appName = 'دفتر الأستاذ المحاسبي';
-  static const String pinKey = 'user_pin_code';
-  static const String authStatusKey = 'auth_status';
-  static const String typeDebit = 'debit';   // لي
-  static const String typeCredit = 'credit'; // علي
-}
