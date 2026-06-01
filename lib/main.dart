9f6889ee612a0689ff6a1a93f5c78aed8ab5f1aa
mishmish20052-cloud <mishmish20052@gmail.com>
2026-06-01 13:43:07 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ void main() async {
   runApp(
     // تغليف التطبيق بالكامل داخل ProviderScope لتفعيل إدارة الحالة الذكية عبر Riverpod
     const ProviderScope(
-      child: App(),
+      child: MyApp(),
     ),
   );
 }
